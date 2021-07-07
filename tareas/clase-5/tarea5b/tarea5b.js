1e0787e823f9eddc4cb1a18cc51f0e069fa0618e
BrenMagallanes <bren.magallanes05@gmail.com>
2021-07-07 01:46:37 -0300

    Crea las variables para la informacion del usuario

--- tareas/clase-5/tarea5b/tarea5b.js
+++ tareas/clase-5/tarea5b/tarea5b.js
@@ -1,4 +1,9 @@
 const $botonAceptar = document.querySelector('#boton-aceptar');
 
 $botonAceptar.onclick = function() {
+    const primerNombreUsuario = document.querySelector('#primer-nombre-usuario').value 
+    const segundoNombreUsuario = document.querySelector('#segundo-nombre-usuario').value
+    const apellidoUsuario = document.querySelector('#apellido-usuario').value
+    const edadUsuario = document.querySelector('#edad-usuario').value
+
 }
