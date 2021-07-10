b8a8c5d44363861612cce776cf611f82f9b9743e
BrenMagallanes <bren.magallanes05@gmail.com>
2021-07-09 21:26:35 -0300

    Crea las variables para las horas, los minutos y los segundos

--- tareas/clase-5/tarea5c/tarea5c.js
+++ tareas/clase-5/tarea5c/tarea5c.js
@@ -1,4 +1,7 @@
 const $calcularTiempoVideos = document.querySelector('#boton-calcular-total');
 
 $calcularTiempoVideos.onclick = function(){
+    const $horasDeVideo = document.querySelectorAll('.horas-de-clase')
+    const $minutosDeVideo = document.querySelectorAll('.minutos-de-clase')
+    const $segundosDeVideo = document.querySelectorAll('.segundos-de-clase')
 }
