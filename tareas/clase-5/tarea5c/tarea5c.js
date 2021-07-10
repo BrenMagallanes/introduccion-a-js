33510dbbce04b3d149e7c58ad8fe8dd66b114651
BrenMagallanes <bren.magallanes05@gmail.com>
2021-07-09 21:29:22 -0300

    Crea variables para acumular los valores

--- tareas/clase-5/tarea5c/tarea5c.js
+++ tareas/clase-5/tarea5c/tarea5c.js
@@ -5,4 +5,8 @@ $calcularTiempoVideos.onclick = function(){
     const $minutosDeVideo = document.querySelectorAll('.minutos-de-clase')
     const $segundosDeVideo = document.querySelectorAll('.segundos-de-clase')
     const $mostrarTiempoTotal = document.querySelector('#tiempo-total')
+    let horasTotal = 0
+    let minutosTotal = 0
+    let segundosTotal = 0
+    
 }
